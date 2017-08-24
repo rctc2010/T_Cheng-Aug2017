4b600fe5f61221594031a045b0f91e11bab2c626
T_Cheng-Aug2017 <cheng.teresa22@gmail.com>
2017-08-24 09:33:43 -0700

    Hello

---
+++ Project2/src/Project2.java
@@ -0,0 +1,16 @@
+//Teresa Cheng
+/* August 24, 2017
+ This class compares output using print and println
+ */
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		System.out.print("Hello");
+		System.out.println("Hello 2");
+		System.out.print("Hello 3");
+
+		System.out.println("Hello 4");
+}
+
+}
