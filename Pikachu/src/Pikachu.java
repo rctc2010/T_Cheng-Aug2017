e292ff73a07f9db691e46223261b1228957a62bb
T_Cheng-Aug2017 <cheng.teresa22@gmail.com>
2017-08-30 10:48:09 -0700

    Pikachu

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,12 @@
+
+public class Pikachu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Pikachu welcomes you to the worlf of Pokemon!");
+			System.out.println(			"(\\_ _/);");
+			System.out.println("(o^ . ^)");
+			
+			}
+
+}
