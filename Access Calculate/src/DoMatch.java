b6d4dbc6f01f8b94c466da5a6286f08ca29f0361
T_Cheng-Aug2017 <cheng.teresa22@gmail.com>
2017-09-06 10:49:41 -0700

    calculate library

---
+++ Access Calculate/src/DoMatch.java
@@ -0,0 +1,16 @@
+/*Teresa Cheng
+	* September 6, 2017
+	*This is the runner for the calculate library, It is used to test the methods in calculate.
+	*/
+public class DoMatch {
+
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(5));
+		System.out.println(Calculate.cube(2));
+		System.out.println(Calculate.average(5));
+
+	}
+
+}
+
+	
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,26 @@
+/*Teresa Cheng
+	*September 6, 2017
+	* Our self written math library
+	* It contains a series of methods to do basic math functions.
+	*/
+public final class Calculate {
+
+	//square the input
+	public static int square (int oprand)  {
+		int squared =oprand*oprand;
+		return squared;
+	}
+
+
+	// cube the input
+	public static int cube (int oprand) {
+		int cube =oprand*oprand*oprand;
+		return cube; 
+	}
+
+	//average the input 
+	public static double average (double oprand) {
+		double average =(oprand+oprand+oprand)/3;
+		return average;
+	}
+}
