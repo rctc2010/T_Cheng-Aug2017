eef88286a80947f83039160c3fafe81560e516d1
T_Cheng-Aug2017 <cheng.teresa22@gmail.com>
2017-09-07 09:50:42 -0700

    9/7/17

--- Access Calculate/src/DoMatch.java
+++ Access Calculate/src/DoMatch.java
@@ -7,7 +7,7 @@ public class DoMatch {
 	public static void main(String[] args) {
 		System.out.println(Calculate.square(5));
 		System.out.println(Calculate.cube(2));
-		System.out.println(Calculate.average(5));
+		System.out.println(Calculate.average(2));
 
 	}
 
