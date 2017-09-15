7a126075012776b14429026b9aef1d54052f6f03
Teresacheng <Teresacheng@10.5.5.115>
2017-09-15 13:48:01 -0700

    Done with part 2

--- Access Calculate/src/DoMatch.java
+++ Access Calculate/src/DoMatch.java
@@ -7,8 +7,21 @@ public class DoMatch {
 	public static void main(String[] args) {
 		System.out.println(Calculate.square(5));
 		System.out.println(Calculate.cube(2));
-		System.out.println(Calculate.average(2));
-
+		System.out.println(Calculate.average(4.0, 1.0));
+		System.out.println(Calculate.average(1.5, 2.5, 3.0));
+		System.out.println(Calculate.toDegrees(6.28));
+		System.out.println(Calculate.toRadians(360.0));
+		System.out.println(Calculate.discriminant(1.0, 9.0, 3.0));
+		System.out.println(Calculate.toImproperFrac(5, 1, 2));
+		System.out.println(Calculate.toMixedNum(11, 2));
+		System.out.println(Calculate.foil(2, 3, 6, -7, "n"));
+		System.out.println(Calculate.isDivisibleBy(8, 2));
+		System.out.println(Calculate.absValue(1.5));
+		System.out.println(Calculate.max(1.5, 3.5));
+		System.out.println(Calculate.max(1.5, 3.5, 5.5));
+		System.out.println(Calculate.min(10, 5));
+		System.out.println(Calculate.round2);
+		
 	}
 
 }
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -19,9 +19,137 @@ public final class Calculate {
 	}
 
 	//average the input 
-	public static double average (double oprand) {
-		double average =(oprand+oprand)/2;
+	public static double average (double oprand, double number) {
+		double average =(oprand+number)/2;
 		return average;
 	}
+
+
+	//average the input
+	public static double average (double oprand, double number, double oprand2) {
+		double average =(oprand+number+oprand2)/2;
+		return average;
+	}
+	
+	//toDegrees
+	public static double toDegrees (double oprand) {
+		double toDegrees =(oprand*180)/3.14;
+		return toDegrees;
+	}
+	
+	//toRadians
+	public static double toRadians (double oprand) {
+		double toRadians =(oprand*3.14)/180;
+		return toRadians;
+	}
+	
+	//discriminant
+	public static double discriminant (double oprandA, double oprandB, double oprandC) {
+		double discriminant =((oprandB*oprandB)-(4*oprandA*oprandC));
+		return discriminant;
+	}
+	
+	//toImproperFrac
+	public static String toImproperFrac (int number, int numerator, int denominator) {
+		int toImproperFrac =(number*denominator+numerator);
+		return toImproperFrac + "/" + denominator;
+		
+	}
+	
+	//toMixedNum
+	public static String toMixedNum (int numerator, int denominator) {
+		int x =(numerator/denominator);
+		int y =(numerator%denominator);
+		String toMixedNum = x + "  " + (y + "/" + denominator);
+		return toMixedNum; 
+	
+	}
+	
+	//foil
+	public static String foil (int a, int b, int c, int d, String x) {
+		int term1 =(a * c);
+		int term2 =(a * d)+(b * c);
+		int term3 =(b * d);
+		String foil = term1 + x +"^2+"+ term2 + x + "+"+ term3 ;
+		return foil; 
+	
+	}
+	
+	/*
+	 *Determines whether or not one integer 9is evenly divisible by another.
+	 *Accepts two integers and returns a boolean.
+	 */
+	public static boolean isDivisibleBy (int num1, int num2) {
+		if (num2 == 0 || num1 == 0) {
+			throw new IllegalArgumentException("divisor : 0");
+		}
+		if (num1 % num2 == 0 || num2 % num1 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+		
+	/*
+	 * Returns the absolute value of the number passed.
+	 * Accepts a double, returns a double.
+	 */
+	public static double absValue (double a) {
+		if (a > 0) {
+			return a;
+		} else {
+			return a*-1;
+		}
+	
+	}
+	
+	/* Returns the larger of the values passed.
+	 * Accepts two doubles and returns a double.
+	 */
+	public static double max (double num1, double num2) {
+		if (num1 > num2) {
+			return num1;
+		} else { 
+			return num2;
+		}
+	}
+	
+	/* Accepts three doubles and returns a double.
+	 */
+	public static double max (double number1, double number2, double number3) {
+		if (number1 > number2 && number1 > number3) {
+			return number1;
+		} else if (number2 > number1 && number2 > number3) {
+			return number2;
+		} else {
+			return number3;
+		}
+	}
+	
+	/* Returns the smaller of the values passed.
+	 * Accepts two integers; returns an integer.)
+	 */
+	public static int min (int num1, int num2) {
+		if (num1 < num2) {
+			return num1;
+		} else {
+			return num2;
+		}
+	}
+	
+	/* Rounds a double correctly to 2 decimal places and returns a double. 
+	 */
+	public static double round2 (double value) {
+		value = (value * 100) + 0.5;
+		value = (int) value;
+		value = (double)value;
+		value = value/100;
+		return value;
+		
+	}
+}
+	
+	
+	
 
 	
