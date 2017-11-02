5a1ca596d68b3418bb92a93153538f1dcb58c016
Teresacheng <Teresacheng@10.5.5.84>
2017-11-02 00:59:29 -0700

    Finished Array Minilab

---
+++ Array Mini-Lab/src/LotsOfCopies.java
@@ -0,0 +1,22 @@
+//Teresa Cheng
+//November 1, 2017
+//Period 2
+import java.util.Arrays;
+
+public class LotsOfCopies {
+	public static void main(String[] args){
+		int num = 7;
+		String strMain = "Hello";
+		int[] arrMain = {1, 2, 3, 4, 5};
+	}
+	public static void changeMe(int x, String str, int[] arr){
+		int num = x;
+		x = 6;
+		String strMain = str;
+		str = "Hello World";
+		arr = new int[5];
+	}
+
+}
+
+
