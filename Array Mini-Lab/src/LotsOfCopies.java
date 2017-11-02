3cd6edc23225b6143d522389094c0eccaf9bd891
Teresacheng <Teresacheng@10.5.5.84>
2017-11-02 09:09:22 -0700

    Revised Minilab

--- Array Mini-Lab/src/LotsOfCopies.java
+++ Array Mini-Lab/src/LotsOfCopies.java
@@ -6,14 +6,14 @@ import java.util.Arrays;
 public class LotsOfCopies {
 	public static void main(String[] args){
 		int num = 7;
-		String strMain = "Hello";
+		String strMain = "APCS";
 		int[] arrMain = {1, 2, 3, 4, 5};
 	}
 	public static void changeMe(int x, String str, int[] arr){
 		int num = x;
 		x = 6;
 		String strMain = str;
-		str = "Hello World";
+		str = "APCS at Mills";
 		arr = new int[5];
 	}
 
